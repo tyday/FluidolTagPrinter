edc893980c0cb1321844ea3fdc74b42c66a658d0
Ty Day <tyrday@gmail.com>
2021-03-04 18:50:31 -0500

    Fix: Moved settings to separate file

--- write_char_to_g_code.py
+++ write_char_to_g_code.py
@@ -1,105 +1,14 @@
 import json, os, subprocess,sys
-
-CWD = sys.path[0]
-OUTPUT_FILE = 'g_code_output.ngc'
-OUTPUT_FILE_PATH = os.path.join(CWD, OUTPUT_FILE)
-SPACING = 0.0125
-CENTERED = True
-Z_HEIGHT_ENGAGED = -0.005
-Z_HEIGHT_DISENGAGED = 0.25
+from SETTINGS import CENTERED, OUTPUT_FILE, SPACING, TAG_DEFINITIONS, TAG_TYPES, Z_HEIGHT_DISENGAGED, Z_HEIGHT_ENGAGED
 
 current_gcode = []
-
+CWD = sys.path[0]
+OUTPUT_FILE_PATH = os.path.join(CWD, OUTPUT_FILE)
 POSITIONS_ENUM = {
     0:'style',
     1:'serial',
     2:'shaft'
 }
-TAG_TYPES = [
-    ("Tag_large", "Large Tag"),
-    ("Tag_small", "Small Tag"),
-    ("Tag_special", "Special Tag"),
-]
-TAG_DEFINITIONS = {
-    "Tag_large": {
-        'name': 'Large Tag',
-        'local_origin': [
-            {'x':0.5,'y':0.5},
-        ],
-        'slots': [
-            'style',
-            'shaft',
-            'serial'
-        ],
-        'style': {
-            'y': 0.3346457,
-            'x1': 0.7480315,
-            'x2': 2.515748
-        },
-        'serial': {
-            'y': 0.1968504,
-            'x1': 0.6692913,
-            'x2': 1.377953
-        },
-        'shaft': {
-            'y': 0.1968504,
-            'x1': 1.889764,
-            'x2': 2.519685
-        },
-        'font': 'isocp2__0p11.json'
-    },
-    "Tag_small": { 
-        'name': 'Small Tag',
-        'local_origin': [
-            {'x':3.75,'y':0.5},
-        ],   
-        'slots': [
-            'style',
-            'shaft',
-            'serial'
-        ],
-        'style': {
-            'y': 0.2952756,
-            'x1':0.7283465,
-            'x2': 2.125984
-        },
-        'serial': {
-            'y': 0.1574803,
-            'x1': 0.5905512,
-            'x2': 1.259843
-        },
-        'shaft': {
-            'y': 0.1574803,
-            'x1': 1.653543,
-            'x2': 2.283465
-        },
-        'font': 'isocp2__0p10.json'
-    },
-    "Tag_special": {
-        'name': 'Special Tag',
-        'local_origin': [
-            {'x':6.75, 'y':0.5},
-        ],
-        'slots': [
-            'style',
-            'serial'
-        ],
-        'style': {
-            'y': 0.5211,
-            'x1': 0.0572,
-            'x2': 1.1038
-        },
-        'serial': {
-            'y': 0.08267717,
-            'x1': 0.0572,
-            'x2': 1.1038
-        },
-        'font': 'isocp2__0p11.json'
-    }
-}
-
-
-
 
 
 def handle_cnc_move(code, current_location, current_gcode):
